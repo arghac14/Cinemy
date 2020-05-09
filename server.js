43a2a739184b14b7648ca2ef1c069ddcdf7ba01d
arghac14 <chakrabortyarghadip5@gmail.com>
2020-05-09 20:23:33 +0530

    fixed port

--- server.js
+++ server.js
@@ -19,4 +19,5 @@ app.get('/search', search);
 app.get('/watchList', watchList);
 app.get('/details', details);
 
-app.listen(5050);
+const PORT = process.env.PORT || 5050;
+app.listen(PORT , ()=> console.log(`Server started on port ${PORT}`));
